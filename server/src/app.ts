40cfbb096334e3b195d0b285230930240bc6cdde
Areco892 <ahuangliu@ucsd.edu>
2026-03-01 16:21:36 -0800

    Organized the workout planner file structure.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import todoRoutes from "./routes/todo.routes";
-import timerRoutes from "./routes/timer.routes";
 import workoutRoutes from "./routes/workout.routes";
 import exerciseRoutes from "./routes/exercise.routes";
 import workoutPlanRoutes from "./routes/workout_plan.routes";
@@ -14,8 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use("/todos", todoRoutes);
-app.use("/timers", timerRoutes);
 app.use("/workouts", workoutRoutes);
 app.use("/exercises", exerciseRoutes);
 app.use("/workoutplans", workoutPlanRoutes);
